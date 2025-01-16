6f084671406fb8e6d372d3d9258fab307def34e1
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2025-01-16 21:23:08 +0100

    Update background gradient

--- app/src/components/ui/DiamondBackground.tsx
+++ app/src/components/ui/DiamondBackground.tsx
@@ -1,31 +1,52 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import Svg, { Polygon, Defs, Filter, FeGaussianBlur } from "react-native-svg";
+import { View, StyleSheet, Dimensions } from "react-native";
+import Svg, {
+  Polygon,
+  Rect,
+  Defs,
+  Filter,
+  FeGaussianBlur,
+} from "react-native-svg";
 import { Colors } from "../../styles/commonStyles";
 
 const DiamondBackground: React.FC = () => {
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height - 200;
+
   return (
     <View style={styles.diamondContainer}>
-      <Svg width="100%" height="100%" viewBox="0 0 200 200">
+      <Svg width="100%" height="100%" color-interpolation-filters="sRGB">
         <Defs>
           <Filter id="blurFilterOuter">
             <FeGaussianBlur in="SourceGraphic" stdDeviation="50" />
           </Filter>
           <Filter id="blurFilterInner">
-            <FeGaussianBlur in="SourceGraphic" stdDeviation="25" />
+            <FeGaussianBlur in="SourceGraphic" stdDeviation="30" />
           </Filter>
         </Defs>
 
         <Polygon
-          points="100,0 200,100 100,200 0,100"
+          points={`
+          ${windowWidth / 2},0
+          ${windowWidth},${windowHeight / 2}
+          ${windowWidth / 2},${windowHeight}
+          0,${windowHeight / 2}
+        `}
           fill={Colors.backgroundGradientMid}
           opacity={0.5}
           filter="url(#blurFilterOuter)"
         />
+
         <Polygon
-          points="100,50 150,100 100,150 50,100"
+          // points="100,50 150,100 100,150 50,100"
+          points={`
+    ${windowWidth / 2},${windowHeight / 2 - windowHeight / 4}
+    ${windowWidth / 2 + windowWidth / 4},${windowHeight / 2}
+    ${windowWidth / 2},${windowHeight / 2 + windowHeight / 4}
+    ${windowWidth / 2 - windowWidth / 4},${windowHeight / 2}
+  `}
           fill={Colors.backgroundGradientStart}
-          opacity={0.3}
+          opacity={0.1}
           filter="url(#blurFilterInner)"
         />
       </Svg>
