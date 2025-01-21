d40dd9d7616f23937d9e2752fdc953bade19aa94
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2025-01-21 18:41:47 +0100

    Update env.d.ts

--- app/src/types/env.d.ts
+++ app/src/types/env.d.ts
@@ -1,4 +1,17 @@
 declare module "@env" {
   export const REACT_APP_IOS_CLIENT_ID: string;
   export const REACT_APP_WEB_CLIENT_ID: string;
+  export const REACT_APP_WEB_CLIENT_ID: string;
+  export const FIREBASE_API_KEY: string;
+  export const FIREBASE_AUTH_DOMAIN: string;
+  export const FIREBASE_PROJECT_ID: string;
+  export const FIREBASE_STORAGE_BUCKET: string;
+  export const FIREBASE_MESSAGING_SENDER_ID: string;
+  export const FIREBASE_APP_ID: string;
+  export const FIREBASE_MEASUREMENT_ID: string;
+
+  export const AGORA_APP_ID: string;
+  export const AGORA_CHANNEL_NAME: string;
+  export const AGORA_TOKEN: string;
+  export const AGORA_UID: string;
 }
